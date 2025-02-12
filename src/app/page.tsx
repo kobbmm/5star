e998be9f398730124e646471551157621b5f4beb
axeaoey <aoey158z@gmail.com>
2025-02-12 17:37:54 +0700

    Homeที่เสร็จ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import { FC } from 'react';
 import Link from 'next/link';
 import HeaderTop from '@/components/Home/HeaderTop';
 import HeaderSection from '@/components/Home/HeaderSection';
+import Headerdetails from '@/components/Home/Headerdetails';
+import HeaderChef from '@/components/Home/HeaderChef';
+import Chefrecommended from '@/components/Home/Chefrecommended';
+import TypesDesserts from '@/components/Home/TypesDesserts';
+import AllMenu from '@/components/Home/AllMenu';
+import Contact from '@/components/Home/Contact';
 
 function Home() {
   return (
@@ -9,7 +15,14 @@ function Home() {
     <main className="min-h-screen bg-gray-50">
       <HeaderTop />
       <HeaderSection />
-
+      <Headerdetails />
+      <HeaderChef />
+      <Chefrecommended />
+      <TypesDesserts />
+      <AllMenu />
+      {/* Reviews Page */}
+      {/* Graph Reviews Page */}
+      <Contact />
     </main>
   );
 }
---
+++ src/components/Home/Headerdetails.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import Image from 'next/link';
+
+const Headerdetails: React.FC = () => {
+    return (
+      <div className="relative w-full h-[300px] bg-[#AB3434] flex flex-col items-center justify-center p-4">
+        <h1 className="absolute top-10 left-20 text-white font-[Aboreto] text-[45px] leading-[52px] mb-4">
+          Cloud & Crème
+        </h1>
+        <p className="absolute left-20 bottom-20 text-black font-[Lato] text-[18px] leading-[22px] font-light w-[500px] text-left">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </p>
+      </div>
+    );
+  };
+  
+  export default Headerdetails;
